f96b7e065f9f5d6b8bda94dbd781c87e6524a773
PeterMPhillips <github@pmphillips.ca>
2023-06-12 14:08:50 -0700

    Token out validation

--- src/EnsoShortcutRouter.sol
+++ src/EnsoShortcutRouter.sol
@@ -1,14 +1,62 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.16;
 
-import { EnsoWalletFactory, EnsoWallet } from "shortcuts-contracts/EnsoWalletFactory.sol";
+
+import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
+
+interface IEnsoWalletFactory {
+    function deploy(bytes32, bytes32[] calldata, bytes[] calldata) external returns (IEnsoWallet);
+}
+
+interface IEnsoWallet {
+    function executeShortcut(bytes32, bytes32[] calldata, bytes[] calldata) external payable returns (bytes[] memory);
+}
 
 contract EnsoShortcutRouter {
-    EnsoWalletFactory public constant factory = EnsoFactory(0x7fEA6786D291A87fC4C98aFCCc5A5d3cFC36bc7b);
-    IEnsoWallet public immutable wallet;
+    address public constant ETH = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+
+    IEnsoWalletFactory public constant FACTORY = IEnsoWalletFactory(0x7fEA6786D291A87fC4C98aFCCc5A5d3cFC36bc7b);
+    IEnsoWallet public immutable WALLET;
+
+    error WrongValue();
+    error AmountTooLow();
+    error TransferFailed();
+
     constructor() {
-        wallet = factory.deploy(bytes32(0), [], []);
+        WALLET = FACTORY.deploy(bytes32(0), new bytes32[](0), new bytes[](0));
     }
 
+    function route(
+        address tokenIn,
+        uint256 amountIn,
+        bytes32[] calldata commands,
+        bytes[] calldata state
+    ) public payable returns (bytes[] memory returnData) {
+        if (tokenIn == ETH) {
+            if (msg.value != amountIn) revert WrongValue();
+        } else {
+            if (msg.value != 0) revert WrongValue();
+            IERC20(tokenIn).transferFrom(msg.sender, address(WALLET), amountIn);
+        }
+        returnData = WALLET.executeShortcut{value: msg.value}(bytes32(0), commands, state);
+    }
 
+    function safeRoute(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint256 minAmountOut,
+        bytes32[] calldata commands,
+        bytes[] calldata state
+    ) external payable returns (bytes[] memory returnData) {
+        uint256 balance = tokenOut == ETH ? msg.sender.balance : IERC20(tokenOut).balanceOf(msg.sender);
+        returnData = route(tokenIn, amountIn, commands, state);
+        uint256 amountOut;
+        if (tokenOut == ETH) {
+            amountOut = msg.sender.balance - balance;
+        } else {
+            amountOut = IERC20(tokenOut).balanceOf(msg.sender) - balance;
+        }
+        if (amountOut < minAmountOut) revert AmountTooLow();
+    }
 }
